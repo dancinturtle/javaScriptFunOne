f9cbd6d2d0d5e0d54fb4087465296989a76dbbf1
Keith Nguyen <khoacn@gmail.com>
2017-10-23 16:06:15 -0700

    Keith's Javascrip Assignment

---
+++ KeithAssignment/Birthday.js
@@ -0,0 +1,14 @@
+var daysUntilMyBirthday =60;
+
+while(daysUntilMyBirthday>=30){
+  console.log(daysUntilMyBirthday +" Days until my birthday Such a long time... :( ");
+  daysUntilMyBirthday--;
+}
+
+for (var daysUntilMyBirthday=29; daysUntilMyBirthday>=5 ; daysUntilMyBirthday--){
+  console.log(daysUntilMyBirthday + " Days until my birthday...WOOH!!");
+}
+for (var daysUntilMyBirthday=4; daysUntilMyBirthday>=1; daysUntilMyBirthday--){
+  console.log(daysUntilMyBirthday + " Days until my birthday...ALMOST HERE!! :P");
+}
+console.log("IT'S MY BIRTHDAY!!!");
